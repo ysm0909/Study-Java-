8487ab78d450d9c5e9f360eda5a289a54e3ebc0a
unknown <ysmm0909@naver.com>
2024-04-14 17:05:08 +0900

    spring

---
+++ SpringExample/RESTExample.java
@@ -0,0 +1,34 @@
+package kr.cbnu.lesson10;
+
+import kr.cbnu.lesson10.dto.TestDto;
+import kr.cbnu.lesson10.model.TestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/v1")
+public class RESTExample {
+    private TestService service;
+
+    public RESTExample(TestService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/visit")
+    public ResponseEntity<TestDto> test() {
+        try {
+            if (service.isApiEndpointEnabled()) {
+                int visitCount = service.getVisitCount(true);
+                TestDto dto = new TestDto(visitCount);
+                return ResponseEntity.ok(dto);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
